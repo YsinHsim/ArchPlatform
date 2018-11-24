4ba2e64c52e2cbeb01912c10632615f729fb2392
YsinHsim <yasinhassim43@gmail.com>
2018-11-24 17:42:37 +0800

    My file...

---
+++ index.php
@@ -0,0 +1,121 @@
+<html lang="en">
+	<head>
+		<title>
+			Arch Platform
+		</title>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<!--<link href="css/bootstrap.css" media="all" rel="stylesheet">-->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<style>
+			body {
+				background-color: #13555e;
+				color: white;
+			}
+			img {
+				height: 200px;
+				width: 200px;
+			}
+			h5 {
+				margin-right: 50px;
+			}
+		</style>
+		
+		<link rel="icon" href="logo01.png" />
+	</head>
+	
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#"><b>Arch Platform:</b> Gaming Center</a>
+				</div>
+				<ul class="nav navbar-nav" style="margin-left: 10px;">
+    				<li class="active"><a href="#">Home</a></li>
+    				<li><a href="#joinus">Join Us</a></li>
+					<li><a href="event.php">Event</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contactus">Contact Us</a></li>
+					
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="signup.php"><span class="glyphicon glyphicon-user"></span>  Sign Up</a></li>
+					<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>  Login</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="jumbotron text-center" style="background-color: #041e22;">
+			<h1>Welcome to Arch Platform: Gaming Center Community!</h1>
+			<p>The best pc gaming community in Sarawak.</p>
+		</div>
+		<div class="container" style="margin-top: 40px;">
+			<div class="row">
+				<div class="col-sm-4">
+					<img src="tech.png" align="center" style="margin-left: 30px;">
+					<h3>Best Gaming Hardware!</h3>
+					<p>Illegear Raider with high requirement.</p>
+					<p>Intel 8th Generation Core i5 Processor.
+					Zotac GTX1060 AMP Edition SE 6GB GDDR5</p>
+				</div>
+				<div class="col-sm-4">
+				<img src="fight.png" align="center" style="margin-left: 60px;">
+					<h3>Meet friend and Competitor!</h3>
+					<p>Where you can meet new people and compete with the best skill.</p>
+					<p>Meet your rivals and gather your teammates
+					at our mindblowing event.</p>
+				</div>
+				<div class="col-sm-4">
+				<img src="event.png" align="center" style="margin-left: 60px;">
+					<h3>Hold your own event with style!</h3>
+					<p>Contact us and get the best equipment for your event.</p>
+					<p>With our jaw dropping equipment, your event
+					will be fantastic with the best experience.</p>
+				</div>
+			</div>
+		</div>
+		<div class="container" style="margin-top: 60px;">
+			<div class="row">
+				<div class="col-sm-4" id="about">
+					<img src="organization.png" align="center" style="margin-left: 30px;">
+					<h3>What is Arch Platform?</h3>
+					<p>We are the very first company that
+						build gamer community with full of
+					events and style in Sarawak.</p>
+				</div>
+				<div class="col-sm-4" id="joinus">
+					<img src="employee.png" align="center" style="margin-left: 60px;">
+					<h3>Want to Join Us and Contribute?</h3>
+					<p>Join Us! Become one of us! Send your
+						resume through our official email and
+					help us contribute to community.</p>
+				</div>
+				<div class="col-sm-4" id="contactus">
+					<img src="office-work.png" align="center" style="margin-left: 60px;">
+					<h3>How to Contact Us?</h3>
+					<p>Call us to make your game event or just
+						walk straight to our office. We sure you
+						will walk away satisfy with our service.</p>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid" style="margin-top: 50px; margin-left: 88px;">
+			<div class="nav navbar-left">
+				<h5>Arch Platform: Gaming Center<br><br>
+					Building A, 1st Floor,<br>
+					Batu Kawah, 93250, Kuching.<br>
+					archplatform@gmail.com<br>
+					0123456-32
+				</h5>
+			</div>
+		</div>
+		<div class="container-fluid" style="margin-top: 10px;">
+			<div class="nav navbar-right">
+				<li><a href="admin.php" style="margin-right: 38px;"><span class="glyphicon glyphicon-briefcase"></span>  Admin</a></li>
+			</div>
+		</div>
+			
+		<!--<script rel="js/bootstrap.js"></script>-->
+	</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,36 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-size=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link href="signupstyle.css" type="text/css" rel="stylesheet">
+		<link rel="icon" href="logo01.png" />
+		<title>
+			Signup
+		</title>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.php"><b>Arch Platform:</b> Gaming Center</a>
+				</div>
+			</div>
+		</nav>
+		<div id="divfrm" class="container">
+			<form action="#" method="post">
+				<div><h3><img src="addmale.png">  Signup</h3></div>
+				<label for="user">Username</label>
+				<input id="user" type="text" name="username" placeholder="Enter Username" required><br>
+				<label for="pass">Password</label>
+				<input id="pass" type="password" name="password" placeholder="Enter Password" required><br>
+				<label for="conf">Confirm</label>
+				<input id="conf" type="password" name="confirm" placeholder="Confirm Password" required><br>
+				<input class="btncls" id="btn" type="submit" value="Signup">
+			</form>
+		</div>
+	</body>
+</html>
